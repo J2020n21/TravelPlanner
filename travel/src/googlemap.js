62d93ba60fd12d53b54530bb3617a3b81a6a1cc8
Jiui <jiui0103@naver.com>
2023-08-15 21:57:29 +0900

    Add react project and prepare for code &add Navbar

---
+++ travel/src/googlemap.js
@@ -0,0 +1,23 @@
+import {Wrapper} from "@googlemaps/react-wrapper"
+import { useState, useEffect, useRef } from "react"
+
+
+const GMap = ()=>{
+    const [map, setMap] = useState(null);
+    const ref = useRef();
+
+    useEffect(()=>{
+        const newMap = new window.google.maps.Map(ref.current, {
+            center : { lat: 37.569227, lng: 126.9777256},
+            zoom : 16,
+        });     
+        
+        setMap(newMap);
+    },[])
+
+    return (
+        <div ref={ref} id="map" style={{width:"400px", height: "400px"}}></div>
+    )
+}
+
+export default GMap
