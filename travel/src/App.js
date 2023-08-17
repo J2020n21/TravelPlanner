a7d7163f12ee54e8d69f5461e1e69b8d090713cf
Jiui <jiui0103@naver.com>
2023-08-17 20:48:47 +0900

    Add router

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,46 +1,55 @@
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Navbar, Container, Nav} from 'react-bootstrap'
-import { Router, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import {Router, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 // import {useParams} from 'react-router-dom'
 import './App.css';
 import GMap from './googlemap.js';
+import Landing from './pages/landing.js'
+import Setting from './pages/setting';
+import Map from './pages/map';
+import CountryDropDown from "./components/country";
 import {useState, useEffect} from 'react';
 import {Wrapper, Status} from "@googlemaps/react-wrapper"; //google map
 
+
 function App() {
-  
+  const navigate = useNavigate();
+
   return (
-    <>
+    <div className="App">
     <Navbar bg="light" data-bs-theme="light">
     <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar.Brand onClick={()=>{navigate('/')}}>Start</Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link href="#home">Plan</Nav.Link>
-        <Nav.Link href="#features">Maps</Nav.Link>
-        <Nav.Link href="#pricing">Setting</Nav.Link>
+        <Nav.Link onClick={()=>{navigate('/plan')}}>Plan</Nav.Link>
+        <Nav.Link onClick={()=>{navigate('/map')}}>Maps</Nav.Link>
+        <Nav.Link onClick={()=>{navigate('/setting')}}>Setting</Nav.Link>
       </Nav>
     </Container>
     </Navbar>
 
-
+{/* 
     <Wrapper apiKey={"AIzaSyAY6AUO3bJvykH8YxldX-yppdDiNjJBYrI"}>
         <GMap/>
-    </Wrapper>    
+    </Wrapper>     */}
 
-    <Card className="plan-vertical"></Card>
 
-  {/* <Router>
+
     <Routes>
-      <Route></Route>
-      <Route></Route>
+      <Route path="/" element={<Landing/>}></Route>
+      <Route path="/plan" element={<Card/>}></Route>
+      <Route path="/map" element={<Map/>}></Route>
+      <Route path="/setting" element={<Setting/>}>
+        <Route path="complain" element={<h2>setting-complain page</h2>} />
+      </Route>
+      <Route path="/toStart" element={<h2>basic questions</h2>}></Route>
+{/* add outlet */}
+      <Route path="*" element={<h1>Page Not Exist</h1>}></Route>
       <Route></Route>
     </Routes>
-  </Router> */}
-
-    
-
-</>
-
+  
+  </div>
   );
   
 function Card(prop){
---
+++ travel/src/pages/landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () =>{
+    return(
+        <>
+        <div>랜딩페이지</div>
+        <h1>Landing</h1>
+        </>
+    )
+}
+
+export default Landing
