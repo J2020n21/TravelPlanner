ebc0662b3b7837270a4cec00f5367915ed49b51c
Son Jiui <jiui0103@naver.com>
2023-09-08 20:26:17 +0900

    show image from pixabay ;need to edit for detail

--- travel/src/components/search/search.js
+++ travel/src/components/search/search.js
@@ -1,45 +1,48 @@
-import React, { Component } from "react";
+import React, { useState ,useEffect } from "react";
 import { TextField, SelectField } from "@material-ui/core";
 import axios from 'axios';
-import imageReaults from '../imageResults/imageResult.js';
 import styled from 'styled-components';
+
+// useEffect(()=>{
+//     //배경화면에 이미지를 띄운다
+
+//     return()=>{ //clean up function (가장먼저 실행)
+//         //리프레싱 할 떄 마다 랜덤 단어가 주어지므로
+//         // 그걸 위해 img배열 초기화.
+//         //그리고 나서 get요청을 한다
+//     }});
 
 const Search = () =>{
     const apiKey='36162160-9ad290b2b95fe84e106ba7a08';
+    let [imageURL, setImage] = useState([]);
     let searchStates = {
-        searchText: '',
+        searchText: 'sky', //need to randomize
         amount: 3,
         apiUrl: 'https://pixabay.com/api',
         apiKey: '36162160-9ad290b2b95fe84e106ba7a08',
         images: []
     };
 
-    let imageURL = '';
-   //get req
-        axios.get(`https://pixabay.com/api/?key=${apiKey}&q=sky&image_type=photo&safesearch=true`)
-        .then((res) => {
-            console.log(res.data.hits);
-            imageURL = res.data.hits[0].webformatURL;
-            console.log('imageURL='+imageURL);
-        })
-        .catch(err => console.log(err));
-    
-    let Background = styled.div`
-    background-image: url("${imageURL}");
-    background-size: 100%;
-    `;
+    //get req
+    axios.get(`https://pixabay.com/api/?key=${searchStates.apiKey}&q=sky&image_type=photo&safesearch=true`)
+    .then((res) => {
+        console.log(res.data.hits);
+        setImage(res.data.hits[0].largeImageURL);
+        console.log('imageURL0='+imageURL);
+    })
+    .catch(err => console.log(err));
+            
 
     return(
-        <div>
-            <div style={{ 
-            backgroundImage: `url("${imageURL}")`
-            }}>
-            Hello World
-            </div>
-        </div>
+        <>
+            <div style={{
+                backgroundImage: `url(${imageURL})`,
+                backgroundSize: 'cover',
+                height: '500px',
+                width: '500px'
+            }}></div>
+        </>
     );
   }
 
-//   https://velog.io/@solmii/React%EC%9D%98-%ED%95%A8%EC%88%98%ED%98%95-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8-feat.Hooks
-// pic > query, etc encode
 export default Search;
