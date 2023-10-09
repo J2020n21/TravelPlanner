dbdb7b85aa74c3fc61e8cc5ae09e5c56da7ac718
Jiui <jiui0103@naver.com>
2023-10-09 15:19:21 +0900

    edit navbar (remove profile part)

--- travel/src/components/navbar.js
+++ travel/src/components/navbar.js
@@ -1,61 +1,9 @@
-// import React from "react";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Container, Box, AppBar, Toolbar, Typography, InputBase, makeStyles} from "@material-ui/core";
-// import AirplaneticketIcon from '@mui/icons-material/AirplaneTicket';
-// // import {Navbar, Container, Nav} from 'react-bootstrap'
-// import {Router, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
-
-// const useStyles = makeStyles({
-//     title: {
-//         color: 'white',
-//     },
-
-// })
-
-// const MyNavBar = () => {
-//     const navigate = useNavigate();
-//     const classes = useStyles();
-
-//     return(
-
-//         <AppBar position="static">
-//             <Toolbar>
-//                 <Container>
-//                     <AirplaneticketIcon/>
-//                 <Typography className={classes.title}> trip designer</Typography>
-//                 </Container>
-//                 <Box display="flex">
-//                         <Typography>Plan</Typography>
-//                         <Typography>Map</Typography>
-//                         <Typography>Setting</Typography>
-//                 </Box>
-//             </Toolbar>
-//         </AppBar>
-
-//     // <Appbar position="static">
-//     //     <Container>
-//     //     <Navbar.Brand onClick={()=>{navigate('/')}}>Start</Navbar.Brand>
-//     //     <Nav className="me-auto">
-//     //         <Nav.Link onClick={()=>{navigate('/Home/plan')}}>Plan</Nav.Link>
-//     //         <Nav.Link onClick={()=>{navigate('/Home/map')}}>Maps</Nav.Link>
-//     //         <Nav.Link onClick={()=>{navigate('/Home/setting')}}>Setting</Nav.Link>
-//     //     </Nav>
-//     //     </Container>
-//     // </Appbar>
-//     );
-// }
-
-// export default MyNavBar
-
-//TODO: add onClick={()=>{navigate('/')}}
-
 import * as React from 'react';
 import {AppBar, Box, Toolbar, Typography,Tooltip,
     IconButton, Menu, Container, Avatar,
     MenuItem, Button
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import AdbIcon from '@mui/icons-material/Adb';
 import { useNavigate } from 'react-router-dom';
 
 const pages = ['Plan', 'Map', 'Setting'];
@@ -103,7 +51,7 @@ export default function MyNavBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Planner
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -143,7 +91,7 @@ export default function MyNavBar() {
             </Menu>
 
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+
           <Typography
             variant="h5"
             noWrap
@@ -161,7 +109,7 @@ export default function MyNavBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Planner
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page,n) => (
@@ -174,36 +122,6 @@ export default function MyNavBar() {
                 {page}
               </Button>
             ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
         </Toolbar>
       </Container>
