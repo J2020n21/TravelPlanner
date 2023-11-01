e7cc4bf970ba4721ab1fe7d8e3c14e7313302192
Son Jiui <jiui0103@naver.com>
2023-11-01 16:51:17 +0900

    ai form processing

--- travel/src/api/openAi.js
+++ travel/src/api/openAi.js
@@ -1,6 +1,8 @@
 import React,{useEffect, useState} from 'react'
 import axios from 'axios';
-import {Button, Box} from '@material-ui/core';
+import {Button, Box, Typography,FormControl, InputLabel, Select
+,MenuItem
+} from '@material-ui/core';
 
 
 const apiKey = process.env.REACT_APP_AI_API_KEY;
@@ -9,11 +11,8 @@ const apiUrl = `https://api.openai.com/v1/chat/completions`
 export default function OpenAi() {
     const [mode, setMode] = useState('text') //place,route,text
     const [userInput,setUserInput] = useState('');
-    const [aiAnswer,setAiAnswer] = useState('');
+    const [aiAnswer,setAiAnswer] = useState(null);
     const [click, setClick] = useState(0);
-
-    let prompt = '';
-    // "Good bye";
 
 
     async function callApi(prompt){
@@ -49,10 +48,6 @@ export default function OpenAi() {
             console.log( typeof(answer))//string
             
         });
-
-
-
-
     }
 
 const changeMode = () =>{
@@ -67,26 +62,68 @@ const handleChange = (e) =>{
 }
 
   return (<>
+  {/* 안내문구 */}
     <div>openAi page, mode:{mode}</div>
     <Button onClick={changeMode} variant='outlined'>Change modes</Button>
 {
-    mode == 'text'?
+    mode === 'text'?
     <Box>
         <Box>
-        <textarea placeholder="ask to ai" onChange={e => handleChange(e)}/>
-    <Button variant='outlined' size='small' onClick={()=>{callApi(userInput)}}>Submit</Button>
+        <textarea style={{width:'100%', height:'50px'}} placeholder="ask to ai" onChange={e => handleChange(e)}/>
+        <Button style={{width:'100%'}} variant='outlined' size='small' onClick={()=>{callApi(userInput)}}>Submit</Button>
+        </Box>
+        <Box bgcolor={"#E6E6E6"} style={{width:'100%',height:'200px', padding:'20px', marginTop:'10px'}}>
+        {
+            aiAnswer!= null? aiAnswer:"there's no answer"
+        }
+        </Box>
     </Box>
-    {
-        aiAnswer!= null? aiAnswer:"there's no answer"
-    }</Box>
     : 
-        mode == 'place'? 
+        mode === 'place'? 
         <>
-        <div>places</div>
+        <Typography variant="h4">Ai for Places</Typography>
+
+        <Typography variant="h6">Location?</Typography>
+        {/* 콤보박스/드롭박스 스크롤 고려해보기 */}
+            <FormControl fullWidth>
+            <InputLabel id="place-category">Place</InputLabel>
+            <Select
+                id="id"
+                label="label"
+            >
+                <MenuItem value={"Korea"}>Korea</MenuItem>
+                <MenuItem value={"France"}>Frace</MenuItem>
+            </Select>
+            </FormControl>
+<div style={{height:'20px'}}></div>
+        <Typography variant="h6">What kind of place?</Typography>
+            <FormControl fullWidth>
+            <InputLabel id="place-category">Place</InputLabel>
+            <Select
+                id="id"
+                label="label"
+            >
+                <MenuItem value={"Restaurants"}>Restaurants</MenuItem>
+                <MenuItem value={"Hotels"}>Hotels</MenuItem>
+                <MenuItem value={"Attractions"}>Attractions</MenuItem>
+            </Select>
+            </FormControl>
+
+            <FormControl fullWidth>
+            <InputLabel id="place-atmosphere">Atmosphere</InputLabel>
+            <Select
+                id="id2"
+                label="label"
+            >
+                <MenuItem value={"Comfortable"}>Comfortable</MenuItem>
+                <MenuItem value={"Luxary"}>Luxary</MenuItem>
+                <MenuItem value={"Reasonable"}>Reasonable</MenuItem>
+            </Select>
+            </FormControl>
     
         </>
         :
-            <div>routes</div>
+            <div>plan</div>
 }
     </>
   )
